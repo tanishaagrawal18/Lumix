30da3070072e497f7808897298e418d6c9a54d42
Tanisha Agrawal <agrawaltanisha222@gmail.com>
2025-10-02 12:05:23 +0530

    sidebar completed

---
+++ src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import SideNav from './Partials/SideNav.jsx'
+function Home() {
+    document.title = "Lumix | Home";
+  return (
+    < >
+         <SideNav />
+        <div className='w-[80%] h-full  '>
+
+        </div>
+    
+    </>
+  )
+}
+
+export default Home
